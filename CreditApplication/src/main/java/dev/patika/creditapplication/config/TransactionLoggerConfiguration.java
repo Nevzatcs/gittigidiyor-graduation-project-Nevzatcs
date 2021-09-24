369d5f0dd165b001c8e1a351f51a74596074aad6
Nevzatcs <nevzatsamur@gmail.com>
2021-09-24 15:10:16 +0300

    Required configs for logging implemented.

---
+++ CreditApplication/src/main/java/dev/patika/creditapplication/config/TransactionLoggerConfiguration.java
@@ -0,0 +1,20 @@
+package dev.patika.creditapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class TransactionLoggerConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public TransactionLoggerInterceptor getInterceptor() {
+        return new TransactionLoggerInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(getInterceptor()).addPathPatterns("/**");
+    }
+}
---
+++ CreditApplication/src/main/java/dev/patika/creditapplication/config/TransactionLoggerInterceptor.java
@@ -0,0 +1,23 @@
+package dev.patika.creditapplication.config;
+
+import dev.patika.creditapplication.util.ClientRequestInfo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class TransactionLoggerInterceptor extends HandlerInterceptorAdapter {
+    @Autowired
+    ClientRequestInfo clientRequestInfo;
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        clientRequestInfo.setClientIpAddress(request.getRemoteAddr());
+        clientRequestInfo.setClientUrl(request.getRequestURI());
+        clientRequestInfo.setSessionActivityId(request.getSession().getId());
+        return true;
+    }
+}
