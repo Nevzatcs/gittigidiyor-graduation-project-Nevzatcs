1e7bec640dd9d58e6a59730aa1de14fd78bd20e0
Nevzatcs <nevzatsamur@gmail.com>
2021-09-26 15:21:23 +0300

    CreditResultDTO updated.

--- CreditApplication/src/main/java/dev/patika/creditapplication/dto/CreditResultDTO.java
+++ CreditApplication/src/main/java/dev/patika/creditapplication/dto/CreditResultDTO.java
@@ -12,10 +12,6 @@ import javax.persistence.*;
 @RequiredArgsConstructor
 public class CreditResultDTO {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private double limit;
 
     @Enumerated(EnumType.STRING)
